a5c646f44fad5dd09898734cea73ff721d89a610
2002OID <ykkim308321@gmail.com>
2023-11-23 10:50:39 +0900

    공의 생성 및 스크롤링, 충돌 구현

--- ball.py
+++ ball.py
@@ -3,18 +3,21 @@ import game_world
 import game_framework
 import random
 
+import server
+
+
 class Ball:
     image = None
 
     def __init__(self, x = None, y = None):
         if Ball.image == None:
             Ball.image = load_image('ball21x21.png')
-        self.x = x if x else random.randint(100, 1180)
-        self.y = y if y else random.randint(100, 924)
+        self.x = x if x else random.randint(50, server.background.w - 50)
+        self.y = y if y else random.randint(50, server.background.h - 50)
 
     def draw(self):
-        self.image.draw(self.x, self.y)
-        draw_rectangle(*self.get_bb())
+        self.image.draw(self.x - server.background.window_left, self.y - server.background.window_bottom)
+        #draw_rectangle(*self.get_bb())
 
     def update(self):
         pass
@@ -23,5 +26,7 @@ class Ball:
         return self.x - 10, self.y - 10, self.x + 10, self.y + 10
 
     def handle_collision(self, group, other):
-	    pass
+        match group:
+            case 'boy:ball':
+                game_world.remove_object(self)
 
